d435edeb60178edc2b68cacbd1033445db29f1d3
Charles-Hong520 <chong039@ucr.edu>
2023-05-09 00:15:13 -0700

    lifegrid constructor, setCell, loadTemplate, compareTemplate

--- src/LifeGrid.cpp
+++ src/LifeGrid.cpp
@@ -1,7 +1,9 @@
 #include "../include/LifeGrid.h"
 
-LifeGrid::LifeGrid(int, int) : ILifeGrid() {
-
+LifeGrid::LifeGrid(int w, int h) : ILifeGrid() {
+    this->width = w;
+    this->height = h;
+    this->grid.resize(this->height,std::vector<bool> (this->width,false));
 }
 
 LifeState LifeGrid::getCell(const Vector2& coordinate) const {
@@ -9,15 +11,31 @@ LifeState LifeGrid::getCell(const Vector2& coordinate) const {
 }
 
 void LifeGrid::setCell(const Vector2& coordinate, const LifeState state) {
-
+    this->grid[coordinate.getY()][coordinate.getX()] = state;
 }
 
 void LifeGrid::loadTemplate(const Vector2& offset, const LifeTemplate& lifeTemplate) {
-
+    int oy = offset.getY();
+    int ox = offset.getX();
+    for(int i = 0; i < lifeTemplate.getHeight(); i++) {
+        for(int j = 0; j < lifeTemplate.getWidth(); j++) {
+            this->grid[oy+i][ox+j] = lifeTemplate.getTemplate()[i][j]; 
+        }
+    }
 }
 
 int LifeGrid::compareTemplate(const Vector2& offset, const LifeTemplate& lifeTemplate) const {
-    return -1;
+    int diffCount = 0;
+    int oy = offset.getY();
+    int ox = offset.getX();
+    for(int i = 0; i < lifeTemplate.getHeight(); i++) {
+        for(int j = 0; j < lifeTemplate.getWidth(); j++) {
+            if(this->grid[oy+i][ox+j] != lifeTemplate.getTemplate()[i][j]) {
+                diffCount++;
+            }
+        }
+    }
+    return diffCount;
 }
 
 void LifeGrid::step() {
