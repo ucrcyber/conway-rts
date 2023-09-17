f53149c27fadd7fb09a8d481ce7400915e04f653
NovaSagittarii <novasagittarii@gmail.com>
2023-09-17 12:26:50 -0700

    add human readable formats (for tests & debugging) and binary formats (for network)

--- include/ISerializable.hpp
+++ include/ISerializable.hpp
@@ -7,10 +7,15 @@
 template<typename T>
 class ISerializable {
   public:
-    // virtual ~ISerializable() = 0;
-    
+    // text based serialization methods (cin/cout) 
     friend std::ostream& operator<<(std::ostream &out, const T &rhs);
     friend std::istream& operator>>(std::istream &in, const T &rhs);
+
+    /// @return whether it was successful or not
+    virtual bool SerializeToOstream(std::ostream &out);
+
+    /// @return whether it was successful or not
+    virtual bool ParseFromIstream(std::istream &in);
 };
 
 // https://stackoverflow.com/a/8513537
