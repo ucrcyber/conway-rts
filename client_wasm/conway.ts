cf649de0c7823a2d6eda9a431a1bccb1ca858b3c
NovaSagittarii <novasagittarii@gmail.com>
2023-12-29 22:39:50 -0700

    add basic typing file for conway library
    --embind-emit-tsd doesn't work too well

---
+++ client_wasm/conway.ts
@@ -0,0 +1,40 @@
+/**
+ * Typescript bindings for the emscripten outputs
+ */
+
+import ConwayWasm from "@/lib-wasm";
+
+interface Type<T> extends Function { // eslint-disable-line @typescript-eslint/ban-types
+  new (...args: any[]): T; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
+
+interface Vector2 {
+  x: () => number;
+  y: () => number;
+  set_x: (x: number) => void;
+  set_y: (y: number) => void;
+}
+
+enum LifeState {
+  DEAD,
+  ALIVE,
+}
+
+interface LifeGrid {
+  dimensions: () => Vector2;
+  GetCell: (position: Vector2) => boolean;
+  SetCell: (position: Vector2) => void;
+  ResetCell: (position: Vector2) => void;
+  Load: (payload: LifeGrid, position: Vector2) => void;
+  Compare: (payload: LifeGrid, position: Vector2) => number;
+  Tick: () => void;
+}
+
+interface ConwayLib {
+  Vector2: Type<Vector2>;
+  LifeState: typeof LifeState;
+  LifeGrid: Type<LifeGrid>;
+}
+
+const Conway = (await ConwayWasm()) as ConwayLib;
+export default Conway;
